6cdab5404bd080eced15a601ddd68720c6ad3d12
ByungKwon <kimbk0814@naver.com>
2022-07-16 18:31:33 +0900

    delete meber

--- member.sql
+++
@@ -1,8 +0,0 @@
-use test;
-create table member (
-	number int(11) auto_increment comment '멤버 테이블의 id 값',
-	name varchar(8) comment '이름',
-    id text comment '아이디',
-    pwd text comment '비밀번호',
-    primary key (number)
-);
